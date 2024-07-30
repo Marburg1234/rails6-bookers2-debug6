83a7156ba1261850038ead09894e32550c04096d
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-07-30 07:18:35 +0000

    6-Finish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,22 +1,34 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    # comment = book.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    # byebug
+    # if
+    #   respond_to do |format|
+    #   format.js
+    #   end
+    # else
+    #   render 'books/show'
+    # end
   end
+    # redirect_to book_path(book)
+
 
   def destroy
       # BookComment.find(params[:id]).destroy
-      comment = BookComment.find(params[:id])
-      if comment.user_id == current_user.id
-        comment.destroy
-        redirect_to request.referer
-      else
-        redirect_to book_path(comment.book)
-      end
+      @comment = BookComment.find(params[:id])
+
+      @comment.destroy
+      @book = Book.find(params[:book_id])
+  end
+      #   redirect_to request.referer
+      # else
+      #   redirect_to book_path(comment.book)
+
       # unless文で作成してものの読み込まれるが、削除も同時に実行される⇒redirect_toが2回読み込まれてエラーになる
       # unless comment.user_id == current_user.id
       #   redirect_to book_path(comment.book)
@@ -24,7 +36,7 @@ class BookCommentsController < ApplicationController
       # @comment = BookComment.find(params[:id])
       # @comment.destroy
       # redirect_to request.referer
-  end
+
 
   private
 
