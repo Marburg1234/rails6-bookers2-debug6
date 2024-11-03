011471d486e1dea4ac9a8ced9a507903c0351f45
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-11-03 15:24:38 +0000

    グループ遷移先調整

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -5,7 +5,7 @@ class GroupUsersController < ApplicationController
     group = Group.find(params[:group_id])
     group_user = group.group_users.build(user_id: user.id, group_id: group.id)
     if group_user.save
-      redirect_to groups_path
+      redirect_to request.referer
       flash[:notice] = "グループに参加しました"
     else
       redirect_to request.referer
@@ -16,7 +16,7 @@ class GroupUsersController < ApplicationController
     group_user = GroupUser.find_by(user_id: params[:id], group_id: params[:group_id])
     group_user.destroy
     flash[:notice] = "グループを脱退しました"
-    redirect_to groups_path
+    redirect_to request.referer
   end
 
 end
